4da589a1fbf9dd48d6b3be40d2d7e69179baa998
Hendrik Mans <hendrik.mans@xing.com>
2015-01-30 16:19:22 +0100

    Add Postgres load path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ source /usr/local/share/chruby/auto.fish
 set PATH $PATH /usr/local/mysql/bin
 # set -x DYLD_LIBRARY_PATH /usr/local/mysql/lib $DYLD_LIBRARY_PATH
 set PATH /usr/local/openresty/bin /usr/local/openresty/nginx/sbin $PATH
+set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
 
 alias cxss="fswatch-run . 'xing s s'"
 
