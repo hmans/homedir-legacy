cf3156a20b97cfae1ef7d49fa6e4bf2af7bd75aa
Hendrik Mans <hendrik@mans.de>
2015-02-28 08:03:37 +0100

    Add my own bin to fish path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set PATH $PATH /usr/local/mysql/bin
 # set -x DYLD_LIBRARY_PATH /usr/local/mysql/lib $DYLD_LIBRARY_PATH
 set PATH /usr/local/openresty/bin /usr/local/openresty/nginx/sbin $PATH
 set PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
+set PATH $HOME/bin $PATH
 
 alias cxss="fswatch-run . 'xing s s'"
 
